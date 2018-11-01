685d83779764a9fc457d65fb1a1bfeb72cda1878
mg <mg@flyingff.net>
2018-11-01 18:53:36 +0800

    改了

--- app/src/main/java/cn/edu/xjtu/mylauncher/MainActivity.java
+++ app/src/main/java/cn/edu/xjtu/mylauncher/MainActivity.java
@@ -1,17 +1,20 @@
 package cn.edu.xjtu.mylauncher;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
-import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,20 +23,37 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<ResolveInfo> mApps;
-    private RecyclerView recycleView;
-    private RecyclerAdapter adapter;
+    private GridView gridView;
+    private GridViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recycleView = findViewById(R.id.recyclerView);
+        gridView = findViewById(R.id.gridView);
         loadApps();
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
-        recycleView.setLayoutManager(layoutManager);
-        adapter = new RecyclerAdapter();
-        recycleView.setAdapter(adapter);
-        recycleView.setItemAnimator(new DefaultItemAnimator());
-        System.out.println(mApps);
+
+        adapter = new GridViewAdapter();
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ResolveInfo info = mApps.get(position);
+
+                //该应用的包名
+                String pkg = info.activityInfo.packageName;
+                System.out.println(pkg);
+                //应用的主activity类
+                String cls = info.activityInfo.name;
+
+                ComponentName component = new ComponentName(pkg, cls);
+
+                Intent i = new Intent();
+                i.setComponent(component);
+                startActivity(i);
+
+
+            }
+        });
     }
 
     private void loadApps() {
@@ -42,32 +62,54 @@ public class MainActivity extends AppCompatActivity {
 
         mApps = getPackageManager().queryIntentActivities(mainIntent, 0);
     }
-    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
-
+    interface OnMyItemClickListener{
+        void click(View view, int position);
+        void click(View v);
+    }
+    class GridViewAdapter extends BaseAdapter{
         @Override
-        public RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item_layout, parent, false);
-            return new ViewHolder(view);
+        public int getCount() {
+            return mApps.size();
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            String name = mApps.get(position).activityInfo.loadLabel(getPackageManager()).toString();
-            holder.textView.setText(name);
-            holder.imageView.setImageDrawable(mApps.get(position).activityInfo.loadIcon(getPackageManager()));
+        public Object getItem(int position) {
+            return mApps.get(position);
         }
 
         @Override
-        public int getItemCount() {
-            return mApps == null ? 0 : mApps.size();
+        public long getItemId(int position) {
+            return position;
         }
-        class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item_layout, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            ActivityInfo info = mApps.get(position).activityInfo;
+            holder.imageView.setImageDrawable(info.loadIcon(getPackageManager()));
+            holder.textView.setText(info.loadLabel(getPackageManager()));
+            return convertView;
+        }
+
+        class ViewHolder {
             TextView textView;
             ImageView imageView;
-            public ViewHolder(View itemView) {
-                super(itemView);
-                textView = itemView.findViewById(R.id.appName);
+            ViewHolder(View itemView) {
+               textView = itemView.findViewById(R.id.appName);
                 imageView = itemView.findViewById(R.id.appIcon);
+                /*imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("fgfh");
+                    }
+                });*/
             }
         }
     }
